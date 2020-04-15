7053f03e12a7d52ce937ce4a50e0f1572057b9f2
Uladzimir Bondarau <bondarau@ualberta.ca>
2020-04-15 15:10:32 -0600

    vue playground of online store with *auto appearing button for removing an item from a cart*

---
+++ main.js
@@ -0,0 +1,126 @@
+Vue.component('product', {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true
+    },
+    cart: {
+      type: Array,
+      required: true
+    }
+  },
+  template: `
+    <div class="product">
+      <div class="product-image">
+          <img :src="image">
+      </div>
+
+      <div class="product-info">
+          <h1>{{ title }}</h1>
+          <a :href="link" target="_blank">More products like this</a>
+          <p v-if="inStock">In Stock</p>
+          <p v-else>Out of Stock</p>
+          <p>Shipping: {{ shipping }}</p>
+
+          <ul>
+              <li v-for="detail in details">{{ detail }}</li>
+          </ul>
+
+          <div v-for="(variant, index) in variants"
+              :key="variant.variantId"
+              class="color-box"
+              :style="{ backgroundColor: variant.variantColour }"
+              @click="updateProduct(index)">
+          </div>
+
+          <button @click="addToCart"
+                  :disabled="!inStock"
+                  :class="{ disabledButton: !inStock }">
+                  Add to Cart</button>
+
+          <button @click="removeFromCart"
+                  v-show="isInCart()"
+                  :class="{ disabledButton: !isInCart }">
+                  Remove from Cart</button>
+      </div>
+    </div>
+  `,
+
+  data() {
+    return {
+      product: 'Socks',
+      brand: 'Vue Mastery',
+      selectedVariant: 0,
+      link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+      details: ["80% cotton", "20% polyester", "Gender-neutral"],
+      variants: [
+          {
+              variantId: 2234,
+              variantColour: "green",
+              variantImage: './assets/vmSocks-green.jpg',
+              variantQuantity: 10
+          },
+          {
+              variantId: 2235,
+              variantColour: "blue",
+              variantImage: './assets/vmSocks-blue.jpg',
+              variantQuantity: 0
+          }
+      ]
+    }
+  },
+
+  methods: {
+    addToCart() {
+      this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
+    },
+    removeFromCart() {
+      this.$emit('remove-from-cart', this.variants[this.selectedVariant].variantId)
+    },
+    updateProduct(index) {
+      this.selectedVariant = index
+    },
+    isInCart() {
+      var index = this.cart.indexOf(this.variants[this.selectedVariant].variantId);
+      return index >= 0
+    }
+  },
+
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity
+    },
+    shipping() {
+      if (this.premium) {
+        return "Free"
+      }
+      return 2.99
+    }
+  }
+})
+
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    premium: true,
+    cart: []
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id)
+    },
+    remvoeCart(id) {
+      var index = this.cart.indexOf(id);
+      if (index !== -1) this.cart.splice(index, 1);
+    }
+  }
+})
+
+
