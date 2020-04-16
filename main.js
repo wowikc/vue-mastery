0196d157cea2edb47e26a9ceb345829f73006ee0
Uladzimir Bondarau <bondarau@ualberta.ca>
2020-04-16 10:28:25 -0600

    after lesson 1.10

--- main.js
+++ main.js
@@ -32,8 +32,24 @@ Vue.component('product', {
           <button @click="addToCart"
                   :disabled="!inStock"
                   :class="{ disabledButton: !inStock }">
-                  Add to Cart</button>
+                  Add to Cart
+                   </button>
+
+          <div>
+            <h2>Reviews</h2>
+            <p v-if="!reviews.length">There are no reviews yet.</p>
+            <ul>
+              <li v-for="review in reviews">
+              <p>{{ review.name }}</p>
+              <p>Rating: {{ review.rating }}</p>
+              <p>{{ review.review }}</p>
+              </li>
+            </ul>
+          </div>
+
+          <product-review @review-submitted="addReview"></product-review>
       </div>
+
     </div>
   `,
 
@@ -45,19 +61,20 @@ Vue.component('product', {
       link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
       details: ["80% cotton", "20% polyester", "Gender-neutral"],
       variants: [
-          {
-              variantId: 2234,
-              variantColour: "green",
-              variantImage: './assets/vmSocks-green.jpg',
-              variantQuantity: 10
-          },
-          {
-              variantId: 2235,
-              variantColour: "blue",
-              variantImage: './assets/vmSocks-blue.jpg',
-              variantQuantity: 0
-          }
-      ]
+        {
+          variantId: 2234,
+          variantColour: "green",
+          variantImage: './assets/vmSocks-green.jpg',
+          variantQuantity: 10
+        },
+        {
+          variantId: 2235,
+          variantColour: "blue",
+          variantImage: './assets/vmSocks-blue.jpg',
+          variantQuantity: 0
+        }
+      ],
+      reviews: []
     }
   },
 
@@ -65,11 +82,11 @@ Vue.component('product', {
     addToCart() {
       this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
     },
-    removeFromCart() {
-      this.$emit('remove-from-cart', this.variants[this.selectedVariant].variantId)
-    },
     updateProduct(index) {
       this.selectedVariant = index
+    },
+    addReview(productReview) {
+      this.reviews.push(productReview)
     }
   },
 
@@ -88,6 +105,73 @@ Vue.component('product', {
         return "Free"
       }
       return 2.99
+    }
+  }
+})
+
+Vue.component('product-review', {
+  template: `
+    <form class="review-form" @submit.prevent="onSubmit">
+
+      <p v-if="errors.length">
+        <b>Please correct the following error(s):</b>
+        <ul>
+          <li v-for="error in errors">{{ error }}</li>
+        </ul>
+      </p>
+
+      <p>
+        <label for="name">Name:</label>
+        <input id="name" v-model="name" placeholder="name">
+      </p>
+
+      <p>
+        <label for="review">Review:</label>      
+        <textarea id="review" v-model="review"></textarea>
+      </p>
+
+      <p>
+        <label for="rating">Rating:</label>
+        <select id="rating" v-model.number="rating">
+          <option>5</option>
+          <option>4</option>
+          <option>3</option>
+          <option>2</option>
+          <option>1</option>
+        </select>
+      </p>
+
+      <p>
+        <input type="submit" value="Submit">  
+      </p>
+
+    </form>
+  `,
+  data() {
+    return {
+      name: null,
+      review: null,
+      rating: null,
+      errors: []
+    }
+  },
+  methods: {
+    onSubmit() {
+      if(this.name && this.review && this.rating) {
+        let productReview = {
+          name: this.name,
+          review: this.review,
+          rating: this.rating
+        }
+        this.$emit('review-submitted', productReview)
+        this.name = null
+        this.review = null
+        this.rating = null
+      } else {
+        if(!this.name) this.errors.push("Name required.")
+        if(!this.review) this.errors.push("Review required.")
+        if(!this.rating) this.errors.push("Rating required.")
+      }
     }
   }
 })
