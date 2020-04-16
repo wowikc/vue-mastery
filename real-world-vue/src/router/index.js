4d829a161959ad2d44de54822d19b868682b05cb
Uladzimir Bondarau <bondarau@ualberta.ca>
2020-04-16 16:43:22 -0600

    Routing basics

--- real-world-vue/src/router/index.js
+++ real-world-vue/src/router/index.js
@@ -1,28 +1,31 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import EventCreate from '../views/EventCreate.vue'
+import EventList from '../views/EventList.vue'
+import EventShow from '../views/EventShow.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path: '/event/create',
+    name: 'event-create',
+    component: EventCreate
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: '/',
+    name: 'event-list',
+    component: EventList
+  },
+  {
+    path: '/event',
+    name: 'event-show',
+    component: EventShow
   }
-];
+]
 
 const router = new VueRouter({
   routes
-});
+})
 
-export default router;
+export default router
