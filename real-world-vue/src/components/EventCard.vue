9abe75b70d05182477e84871a01ff86e4da258dd
Uladzimir Bondarau <bondarau@ualberta.ca>
2020-04-17 15:00:14 -0600

    fake axios with database, proper event displaying on opening

--- real-world-vue/src/components/EventCard.vue
+++ real-world-vue/src/components/EventCard.vue
@@ -1,31 +1,21 @@
 <template>
   <router-link
     class="event-link"
-    :to="{ name: 'event-show', params: { id: '1' } }"
+    :to="{ name: 'event-show', params: { id: event.id } }"
   >
     <div class="event-card -shadow">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
       <h4 class="title">{{ event.title }}</h4>
-      <BaseIcon name="users" />
-      <span>{{ event.attendees.length }} attending</span>
+      <BaseIcon name="users">{{ event.attendees.length }} attending</BaseIcon>
     </div>
   </router-link>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      event: {
-        id: 1,
-        title: 'Beach Cleanup',
-        date: 'Tues Aug 19, 2020',
-        time: '6:00',
-        attendees: [
-          { id: 'abc123, name: "Abraham Jahr' },
-          { id: 'def456, name: "Abi Polo' }
-        ]
-      }
+  props: {
+    event: {
+      type: Object
     }
   }
 }
