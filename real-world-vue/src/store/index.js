698c0d93bb9ce0204045751fa619e8fe4aa14325
Uladzimir Bondarau <bondarau@ualberta.ca>
2020-04-19 23:35:08 -0600

    moved the calling of EventService to the store and just using state for info passing. completed lesson 2 of mutations and actions

--- real-world-vue/src/store/index.js
+++ real-world-vue/src/store/index.js
@@ -16,11 +16,22 @@ export default new Vuex.Store({
       'food',
       'community'
     ],
-    events: []
+    events: [],
+    eventsTotal: 0,
+    event: {}
   },
   mutations: {
     ADD_EVENT(state, event) {
       state.events.push(event)
+    },
+    SET_EVENTS(state, events) {
+      state.events = events
+    },
+    SET_EVENTS_TOTAL(state, eventsTotal) {
+      state.eventsTotal = eventsTotal
+    },
+    SET_SHOW_EVENT(state, event) {
+      state.event = event
     }
   },
   actions: {
@@ -28,12 +39,37 @@ export default new Vuex.Store({
       return EventService.postEvent(event).then(() => {
         commit('ADD_EVENT', event)
       })
+    },
+    fetchEvents({ commit }, { perPage, page }) {
+      EventService.getEvents(perPage, page)
+        .then(response => {
+          commit('SET_EVENTS_TOTAL', response.headers['x-total-count'])
+          commit('SET_EVENTS', response.data)
+        })
+        .catch(error => {
+          console.log('There was an error:' + error.response)
+        })
+    },
+    fetchShowEvent({ commit, getters }, event_id) {
+      var event = getters.getEventById(event_id)
+
+      if (event) {
+        commit('SET_EVENT', event)
+      } else {
+        EventService.getEvent(event_id)
+          .then(response => {
+            commit('SET_SHOW_EVENT', response.data)
+          })
+          .catch(error => {
+            console.log('There was an error:', error.response)
+          })
+      }
     }
   },
   modules: {},
   getters: {
-    catLength: state => {
-      return state.categories.length
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
     }
   }
 })
